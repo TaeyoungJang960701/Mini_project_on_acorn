0423813f99cf46a8dd1506d01c1be8ef3ae629f7
bbun <wkoilz1028@gmail.com>
2025-07-21 15:53:58 +0900

    인력사무소 기본 틀 window version

---
+++ django-example-win/HRlist/HRapp/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.HomeView , name='home'),
+    path('login/', views.LoginView , name='login'),
+	path('signup/', views.SignupView , name='signup'),
+    path('members/', views.MembersView, name='members'),
+    path('members/<int:user_id>/', views.MembersDetailView, name='member_detail'),
+    path('members/me', views.MeView, name='me_detail'),
+]
